f2ac5d558552fd06cec2c68648202a88dde2a694
Mark Ridgwell <credfeto@users.noreply.github.com>
2022-05-02 00:42:28 +0100

    Create shoutouts.js

---
+++ shoutouts.js
@@ -0,0 +1,7 @@
+{
+  "jimmynewtron666": "Join @jimmynewtron666's discord for the amazing miniature guitars and ships in bottles https://discord.gg/nWzxuWmbHH",
+  "karenwarbis": "Check out Karen's 1940's and 1960's streams at https://www.twitch.tv/karenwarbis",
+  "philsmythmusic": "Go check out the awesome Phil Smyth. He gets my seal of approval... https://www.twitch.tv/philsmythmusic",
+  "kennysprogrockjukebox", "Check out Kenny's Prog Rock Piano Bar (PRPB) at https://www.twitch.tv/kennysprogrockjukebox and Kenny & Tommy's kRaZy act The Jukesters at https://www.twitch.tv/thejukesters",
+  "thejukesters", "Check out Kenny & Tommy's kRaZy act The Jukesters at https://www.twitch.tv/thejukesters"
+}
